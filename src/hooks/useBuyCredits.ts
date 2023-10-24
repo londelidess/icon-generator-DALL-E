7be8f87c07bd29ed1d14f343ea2aa933fd5df886
makoto <d.makoto0701@yahoo.com>
2023-10-24 06:32:38 -0700

    useBuyCredits

--- src/hooks/useBuyCredits.ts
+++ src/hooks/useBuyCredits.ts
@@ -1,21 +1,19 @@
-import { loadStripe } from '@stripe/stripe-js'
-// import { useBuyCredits } from './useBuyCredits';
-import { env } from "~/env.mjs"
+import { loadStripe } from "@stripe/stripe-js";
+import { env } from "~/env.mjs";
 import { api } from "~/utils/api";
 
+const stripePromise = loadStripe(env.NEXT_PUBLIC_STRIPE_KEY);
 
-const stripePromise = loadStripe(env.NEXT_PUBLIC_STRIPE_KEY)
+export function useBuyCredits() {
+  const checkout = api.checkout.createCheckout.useMutation();
 
-export function useBuyCredits  () {
-    const checkout = api.checkout.createCheckout.useMutation();
-
-    return{
-        buyCredits: async()=>{
-            const response = await checkout.mutateAsync();
-            const stripe = await stripePromise;
-            await stripe?.redirectToCheckout({
-                sessionId: response.id,
-            });
-        },
-    };
+  return {
+    buyCredits: async () => {
+      const response = await checkout.mutateAsync();
+      const stripe = await stripePromise;
+      await stripe?.redirectToCheckout({
+        sessionId: response.id,
+      });
+    },
+  };
 }
