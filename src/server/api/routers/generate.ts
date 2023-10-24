20632ff8e2f3493ed5db484d695956dead7c4900
makoto <d.makoto0701@yahoo.com>
2023-10-24 06:50:04 -0700

    generate.ts

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -2,10 +2,9 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { env } from "~/env.mjs"
+import { Configuration, OpenAIApi } from "openai";
+import { env } from "~/env.mjs";
 import { b64Image } from "~/data/b64Image";
-
-import { Configuration, OpenAIApi } from 'openai';
 import AWS from "aws-sdk";
 
 const s3 = new AWS.S3({
@@ -13,31 +12,26 @@ const s3 = new AWS.S3({
     accessKeyId: env.ACCESS_KEY_ID,
     secretAccessKey: env.SECRET_ACCESS_KEY,
   },
-  region: "us-west-1",
+  region: "us-east-1",
 });
 
-const BUCKET_NAME = "icon-generator-londelidess";
+const BUCKET_NAME = "icon-generator-course";
 
 const configuration = new Configuration({
   apiKey: env.DALLE_API_KEY,
-})
-
+});
 const openai = new OpenAIApi(configuration);
 
-async function generateIcon(prompt:string):Promise<string | undefined>{
-  if(env.MOCK_DALLE==='true'){
+async function generateIcon(prompt: string): Promise<string | undefined> {
+  if (env.MOCK_DALLE === "true") {
     return b64Image;
   } else {
     const response = await openai.createImage({
       prompt,
       n: 1,
       size: "512x512",
-      response_format:"b64_json"
+      response_format: "b64_json",
     });
-
-    // console.log("---")
-    // console.log(response.data.data[0]?.b64_json)
-    // console.log("---")
     return response.data.data[0]?.b64_json;
   }
 }
@@ -48,63 +42,53 @@ export const generateRouter = createTRPCRouter({
       z.object({
         prompt: z.string(),
         color: z.string(),
-        // shape: z.string(),
-        // style: z.string(),
-        // numberOfIcons: z.number().min(1).max(10),
       })
     )
     .mutation(async ({ ctx, input }) => {
-      // console.log("we are here", input.prompt);
-
-      //TO DO: verify the user has enough credit
       const { count } = await ctx.prisma.user.updateMany({
         where: {
-          id: ctx.session.user.id, //TO DO: replace with a real id
+          id: ctx.session.user.id,
           credits: {
-            gte: 1
-          }
+            gte: 1,
+          },
         },
         data: {
           credits: {
             decrement: 1,
           },
         },
-      })
+      });
 
       if (count <= 0) {
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: "You do not have enough credits",
+          message: "you do not have enough credits",
         });
       }
-      // console.log(results);
-      // TO DO: make a fetch request to DALLE api
 
-      const finalPrompt = `a modern ${input.shape} icon in ${input.color} of ${input.prompt}, minimialistic, high quality, trending on art station, unreal engine graphics quality`;
+      const finalPrompt = `a modern icon in ${input.color} of ${input.prompt}, 3d rendered, metallic material, shiny, minimialistic`;
 
-      const base64EncodedImage = await generateIcon(finalPrompt)
+      const base64EncodedImage = await generateIcon(finalPrompt);
 
       const icon = await ctx.prisma.icon.create({
-        data:{
+        data: {
           prompt: input.prompt,
-          userId: ctx.session.user.id
-        }
-      })
+          userId: ctx.session.user.id,
+        },
+      });
 
-      // // TO DO: save the image to the S3 bucket
       await s3
-      .putObject({
-        Bucket: BUCKET_NAME,
-        Body: Buffer.from(base64EncodedImage!,"base64"),
-        Key: icon.id,//TO DO: generate a random ID
-        ContentEncoding:"base64",
-        ContentType:"image/png",
-      })
-      .promise();
+        .putObject({
+          Bucket: BUCKET_NAME,
+          Body: Buffer.from(base64EncodedImage!, "base64"),
+          Key: icon.id,
+          ContentEncoding: "base64",
+          ContentType: "image/png",
+        })
+        .promise();
 
       return {
-        imageUrl: `https://${BUCKET_NAME}.s3.us-west-1.amazonaws.com/${icon.id}`,
-        // imageUrl: base64EncodedImage,
+        imageUrl: `https://${BUCKET_NAME}.s3.amazonaws.com/${icon.id}`,
       };
     }),
 });
