ccfb089961d510c2bfce9b1b71f2d434da00828c
makoto <d.makoto0701@yahoo.com>
2023-10-27 16:58:54 -0700

    clickRef

--- src/pages/frame-list.tsx
+++ src/pages/frame-list.tsx
@@ -10,7 +10,7 @@ import FrameItem from './frame-item'
 
 const FrameList = ({ images }: { images: imagesType[] }) => {
   const frameRef = useRef<THREE.Group>(null)
-  const clickRef = useRef<unknown>(null);
+  const clickRef = useRef<THREE.Object3D | null>(null);
   const [select, setSelect] = useState<string>('/')
   const targetPosition = useMemo(() => new THREE.Vector3(), [])
   const targetQuaternion = useMemo(() => new THREE.Quaternion(), [])
