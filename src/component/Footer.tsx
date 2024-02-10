7eed0d5ef1fac691d073b6c02d855b653af6ee10
londelidess <d.makoto0701@yahoo.com>
2024-02-10 12:57:52 -0800

    added privacy and service pages

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -4,22 +4,43 @@ import { FaLinkedin, FaFacebook, FaGithub } from 'react-icons/fa';
 export function Footer() {
   return (
     <footer className="dark:bg-gray-900">
-      <div className="container mt-2 mx-auto flex flex-col md:flex-row h-24 items-center justify-between w-full px-4">
-        <ul className="flex flex-col md:flex-row justify-between w-full items-center">
-          <li>
-            <PrimaryLink href="/">icons.makotodoi.com</PrimaryLink>
-          </li>
-          <li>
-            © {new Date().getFullYear()} | Coded by Makoto Doi
-          </li>
+      <div className="container mx-auto mt-2 flex h-24 w-full flex-col items-center justify-between px-4 md:flex-row">
+        <ul className="flex w-full flex-col items-center justify-between md:flex-row">
           <li className="flex space-x-4">
-            <a href="https://www.linkedin.com/in/makoto-doi/" target="_blank" rel="noopener noreferrer">
+            <li>
+              <PrimaryLink href="/">icons.makotodoi.com</PrimaryLink>
+            </li>
+            <li>
+
+              <PrimaryLink href="/privacy">Privacy Policy</PrimaryLink>
+            </li>
+            <li>
+              <PrimaryLink href="/service">
+                Terms of Service
+              </PrimaryLink>
+            </li>
+          </li>
+          <li>© {new Date().getFullYear()} | Coded by Makoto Doi</li>
+          <li className="flex space-x-4">
+            <a
+              href="https://www.linkedin.com/in/makoto-doi/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaLinkedin />
             </a>
-            <a href="https://www.facebook.com/profile.php?id=100004164127853" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.facebook.com/profile.php?id=100004164127853"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaFacebook />
             </a>
-            <a href="https://github.com/londelidess" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/londelidess"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaGithub />
             </a>
           </li>
