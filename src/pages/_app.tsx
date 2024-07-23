a5aa13ac7d076789604aeae30957db18bc68e4df
londelidess <d.makoto0701@yahoo.com>
2024-07-23 13:43:52 -0700

    added font as Press Start 2P

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,12 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { Header } from "~/component/Header";
 import { Footer } from "~/component/Footer";
+import { Press_Start_2P } from 'next/font/google';
+
+export const pressStart2P = Press_Start_2P({
+  weight: ['400'],
+  subsets: ['latin'],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,9 +20,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+        <Header />
+        <Component {...pageProps} className={pressStart2P.className}/>
+        <Footer />
     </SessionProvider>
   );
 };
