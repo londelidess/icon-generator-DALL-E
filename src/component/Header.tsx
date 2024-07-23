1f7026f6affd760d97e18e79eead007599e739ab
londelidess <d.makoto0701@yahoo.com>
2024-07-23 09:18:33 -0700

    added gap-4 to the sign isLoggedIn parts in header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -43,7 +43,7 @@ export function Header() {
             </li>
           )}
         </ul>
-        <ul className="flex flex-row">
+        <ul className="flex flex-row gap-4">
           {isLoggedIn && (
             <>
               <div className="flex items-center">
